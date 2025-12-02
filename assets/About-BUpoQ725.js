ec8c8c7985c320caebe7c35c0e13dd4f6156c20c
Harrison Oest <harrison.oest@gmail.com>
2025-12-01 22:41:59 -0700

    Updates

---
+++ assets/About-BUpoQ725.js
@@ -0,0 +1,96 @@
+import{r as o,j as e}from"./index-B-biaQch.js";import{C as i}from"./ContentColumn-Bos5hrJ9.js";const x="/assets/harrison-CeVWLZQH.jpeg";/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var b={outline:{xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"},filled:{xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"currentColor",stroke:"none"}};/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=(s,r,n,t)=>{const c=o.forwardRef(({color:d="currentColor",size:l=24,stroke:g=2,title:p,className:m,children:h,...y},u)=>o.createElement("svg",{ref:u,...b[s],width:l,height:l,className:["tabler-icon",`tabler-icon-${r}`,m].join(" "),strokeWidth:g,stroke:d,...y},[p&&o.createElement("title",{key:"svg-title"},p),...t.map(([k,M])=>o.createElement(k,M)),...Array.isArray(h)?h:[h]]));return c.displayName=`${n}`,c};/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const N=[["path",{d:"M17 18.5a15.198 15.198 0 0 1 -7.37 1.44a14.62 14.62 0 0 1 -6.63 -2.94",key:"svg-0"}],["path",{d:"M19.5 21c.907 -1.411 1.451 -3.323 1.5 -5c-1.197 -.773 -2.577 -.935 -4 -1",key:"svg-1"}],["path",{d:"M3 11v-4.5a1.5 1.5 0 0 1 3 0v4.5",key:"svg-2"}],["path",{d:"M3 9h3",key:"svg-3"}],["path",{d:"M9 5l1.2 6l1.8 -4l1.8 4l1.2 -6",key:"svg-4"}],["path",{d:"M18 10.25c0 .414 .336 .75 .75 .75h1.25a1 1 0 0 0 1 -1v-1a1 1 0 0 0 -1 -1h-1a1 1 0 0 1 -1 -1v-1a1 1 0 0 1 1 -1h1.25a.75 .75 0 0 1 .75 .75",key:"svg-5"}]],j=a("outline","brand-aws","BrandAws",N);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const B=[["path",{d:"M10 9a3 3 0 0 0 -3 -3h-.5a3.5 3.5 0 0 0 -3.5 3.5v5a3.5 3.5 0 0 0 3.5 3.5h.5a3 3 0 0 0 3 -3",key:"svg-0"}],["path",{d:"M16 7l-1 10",key:"svg-1"}],["path",{d:"M20 7l-1 10",key:"svg-2"}],["path",{d:"M14 10h7.5",key:"svg-3"}],["path",{d:"M21 14h-7.5",key:"svg-4"}]],_=a("outline","brand-c-sharp","BrandCSharp",B);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=[["path",{d:"M22 12.54c-1.804 -.345 -2.701 -1.08 -3.523 -2.94c-.487 .696 -1.102 1.568 -.92 2.4c.028 .238 -.32 1 -.557 1h-14c0 5.208 3.164 7 6.196 7c4.124 .022 7.828 -1.376 9.854 -5c1.146 -.101 2.296 -1.505 2.95 -2.46z",key:"svg-0"}],["path",{d:"M5 10h3v3h-3z",key:"svg-1"}],["path",{d:"M8 10h3v3h-3z",key:"svg-2"}],["path",{d:"M11 10h3v3h-3z",key:"svg-3"}],["path",{d:"M8 7h3v3h-3z",key:"svg-4"}],["path",{d:"M11 7h3v3h-3z",key:"svg-5"}],["path",{d:"M11 4h3v3h-3z",key:"svg-6"}],["path",{d:"M4.571 18c1.5 0 2.047 -.074 2.958 -.78",key:"svg-7"}],["path",{d:"M10 16l0 .01",key:"svg-8"}]],I=a("outline","brand-docker","BrandDocker",f);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const z=[["path",{d:"M21 14l-9 7l-9 -7l3 -11l3 7h6l3 -7z",key:"svg-0"}]],w=a("outline","brand-gitlab","BrandGitlab",z);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const C=[["path",{d:"M15.695 14.305c1.061 1.06 2.953 .888 4.226 -.384c1.272 -1.273 1.444 -3.165 .384 -4.226c-1.061 -1.06 -2.953 -.888 -4.226 .384c-1.272 1.273 -1.444 3.165 -.384 4.226z",key:"svg-0"}],["path",{d:"M12.68 9.233c-1.084 -.497 -2.545 -.191 -3.591 .846c-1.284 1.273 -1.457 3.165 -.388 4.226c1.07 1.06 2.978 .888 4.261 -.384a3.669 3.669 0 0 0 1.038 -1.921h-2.427",key:"svg-1"}],["path",{d:"M5.5 15h-1.5",key:"svg-2"}],["path",{d:"M6 9h-2",key:"svg-3"}],["path",{d:"M5 12h-3",key:"svg-4"}]],L=a("outline","brand-golang","BrandGolang",C);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const S=[["path",{d:"M12 3v19",key:"svg-0"}],["path",{d:"M18 11.227c0 3.273 -1.812 4.77 -6 9.273c-4.188 -4.503 -6 -6 -6 -9.273c0 -4.454 3.071 -6.927 6 -9.227c2.929 2.3 6 4.773 6 9.227z",key:"svg-1"}]],$=a("outline","brand-mongodb","BrandMongodb",S);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const D=[["path",{d:"M13 21c-1.427 -1.026 -3.59 -3.854 -4 -6c-.486 .77 -1.501 2 -2 2c-1.499 -.888 -.574 -3.973 0 -6c-1.596 -1.433 -2.468 -2.458 -2.5 -4c-3.35 -3.44 -.444 -5.27 2.5 -3h1c8.482 .5 6.421 8.07 9 11.5c2.295 .522 3.665 2.254 5 3.5c-2.086 -.2 -2.784 -.344 -3.5 0c.478 1.64 2.123 2.2 3.5 3",key:"svg-0"}],["path",{d:"M9 7h.01",key:"svg-1"}]],E=a("outline","brand-mysql","BrandMysql",D);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const A=[["path",{d:"M9 9v8.044a2 2 0 0 1 -2.996 1.734l-1.568 -.9a3 3 0 0 1 -1.436 -2.561v-6.635a3 3 0 0 1 1.436 -2.56l6 -3.667a3 3 0 0 1 3.128 0l6 3.667a3 3 0 0 1 1.436 2.561v6.634a3 3 0 0 1 -1.436 2.56l-6 3.667a3 3 0 0 1 -3.128 0",key:"svg-0"}],["path",{d:"M17 9h-3.5a1.5 1.5 0 0 0 0 3h2a1.5 1.5 0 0 1 0 3h-3.5",key:"svg-1"}]],R=a("outline","brand-nodejs","BrandNodejs",A);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const P=[["path",{d:"M12 9h-7a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h3",key:"svg-0"}],["path",{d:"M12 15h7a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-3",key:"svg-1"}],["path",{d:"M8 9v-4a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v5a2 2 0 0 1 -2 2h-4a2 2 0 0 0 -2 2v5a2 2 0 0 0 2 2h4a2 2 0 0 0 2 -2v-4",key:"svg-2"}],["path",{d:"M11 6l0 .01",key:"svg-3"}],["path",{d:"M13 18l0 .01",key:"svg-4"}]],T=a("outline","brand-python","BrandPython",P);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const F=[["path",{d:"M6.306 8.711c-2.602 .723 -4.306 1.926 -4.306 3.289c0 2.21 4.477 4 10 4c.773 0 1.526 -.035 2.248 -.102",key:"svg-0"}],["path",{d:"M17.692 15.289c2.603 -.722 4.308 -1.926 4.308 -3.289c0 -2.21 -4.477 -4 -10 -4c-.773 0 -1.526 .035 -2.25 .102",key:"svg-1"}],["path",{d:"M6.305 15.287c-.676 2.615 -.485 4.693 .695 5.373c1.913 1.105 5.703 -1.877 8.464 -6.66c.387 -.67 .733 -1.339 1.036 -2",key:"svg-2"}],["path",{d:"M17.694 8.716c.677 -2.616 .487 -4.696 -.694 -5.376c-1.913 -1.105 -5.703 1.877 -8.464 6.66c-.387 .67 -.733 1.34 -1.037 2",key:"svg-3"}],["path",{d:"M12 5.424c-1.925 -1.892 -3.82 -2.766 -5 -2.084c-1.913 1.104 -1.226 5.877 1.536 10.66c.386 .67 .793 1.304 1.212 1.896",key:"svg-4"}],["path",{d:"M12 18.574c1.926 1.893 3.821 2.768 5 2.086c1.913 -1.104 1.226 -5.877 -1.536 -10.66c-.375 -.65 -.78 -1.283 -1.212 -1.897",key:"svg-5"}],["path",{d:"M11.5 12.866a1 1 0 1 0 1 -1.732a1 1 0 0 0 -1 1.732z",key:"svg-6"}]],Q=a("outline","brand-react","BrandReact",F);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const q=[["path",{d:"M10.139 3.463c.473 -1.95 3.249 -1.95 3.722 0a1.916 1.916 0 0 0 2.859 1.185c1.714 -1.045 3.678 .918 2.633 2.633a1.916 1.916 0 0 0 1.184 2.858c1.95 .473 1.95 3.249 0 3.722a1.916 1.916 0 0 0 -1.185 2.859c1.045 1.714 -.918 3.678 -2.633 2.633a1.916 1.916 0 0 0 -2.858 1.184c-.473 1.95 -3.249 1.95 -3.722 0a1.916 1.916 0 0 0 -2.859 -1.185c-1.714 1.045 -3.678 -.918 -2.633 -2.633a1.916 1.916 0 0 0 -1.184 -2.858c-1.95 -.473 -1.95 -3.249 0 -3.722a1.916 1.916 0 0 0 1.185 -2.859c-1.045 -1.714 .918 -3.678 2.633 -2.633a1.914 1.914 0 0 0 2.858 -1.184z",key:"svg-0"}],["path",{d:"M8 12h6a2 2 0 1 0 0 -4h-6v8v-4z",key:"svg-1"}],["path",{d:"M19 16h-2a2 2 0 0 1 -2 -2a2 2 0 0 0 -2 -2h-1",key:"svg-2"}],["path",{d:"M9 8h-4",key:"svg-3"}],["path",{d:"M5 16h4",key:"svg-4"}]],G=a("outline","brand-rust","BrandRust",q);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const H=[["path",{d:"M15 17.5c.32 .32 .754 .5 1.207 .5h.543c.69 0 1.25 -.56 1.25 -1.25v-.25a1.5 1.5 0 0 0 -1.5 -1.5a1.5 1.5 0 0 1 -1.5 -1.5v-.25c0 -.69 .56 -1.25 1.25 -1.25h.543c.453 0 .887 .18 1.207 .5",key:"svg-0"}],["path",{d:"M9 12h4",key:"svg-1"}],["path",{d:"M11 12v6",key:"svg-2"}],["path",{d:"M21 19v-14a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2 -2z",key:"svg-3"}]],W=a("outline","brand-typescript","BrandTypescript",H);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const U=[["path",{d:"M12 5m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0",key:"svg-0"}],["path",{d:"M17.723 7.41a7.992 7.992 0 0 0 -3.74 -2.162m-3.971 0a7.993 7.993 0 0 0 -3.789 2.216m-1.881 3.215a8 8 0 0 0 -.342 2.32c0 .738 .1 1.453 .287 2.132m1.96 3.428a7.993 7.993 0 0 0 3.759 2.19m4 0a7.993 7.993 0 0 0 3.747 -2.186m1.962 -3.43a8.008 8.008 0 0 0 .287 -2.131c0 -.764 -.107 -1.503 -.307 -2.203",key:"svg-1"}],["path",{d:"M5 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0",key:"svg-2"}],["path",{d:"M19 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0",key:"svg-3"}]],O=a("outline","brand-ubuntu","BrandUbuntu",U);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const V=[["path",{d:"M12 6m-8 0a8 3 0 1 0 16 0a8 3 0 1 0 -16 0",key:"svg-0"}],["path",{d:"M4 6v6a8 3 0 0 0 16 0v-6",key:"svg-1"}],["path",{d:"M4 12v6a8 3 0 0 0 16 0v-6",key:"svg-2"}]],X=a("outline","database","Database",V);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const Z=[["path",{d:"M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1",key:"svg-0"}]],v=a("outline","puzzle","Puzzle",Z);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const J=[["path",{d:"M3 4m0 3a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v2a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3z",key:"svg-0"}],["path",{d:"M3 12m0 3a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v2a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3z",key:"svg-1"}],["path",{d:"M7 8l0 .01",key:"svg-2"}],["path",{d:"M7 16l0 .01",key:"svg-3"}]],K=a("outline","server","Server",J);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const Y=[["path",{d:"M8 9l3 3l-3 3",key:"svg-0"}],["path",{d:"M13 15l3 0",key:"svg-1"}],["path",{d:"M3 4m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z",key:"svg-2"}]],e1=a("outline","terminal-2","Terminal2",Y);function t1(){const s=[{title:"Back End Lead Engineer",company:"Nagrastar, LLC",period:"February 2025 - Present",description:["Design and deploy AWS cloud infrastructure for high-availability production services","Develop responsive React/TypeScript frontend features","Optimize performance of Rust microservices","Lead sprint planning and architectural decisions"]},{title:"Software Developer",company:"Nagrastar, LLC",period:"March 2023 - February 2025",description:["Enhanced Qt applications with critical features","Built and maintained Node.js server for data standardization","Introduced bash automation scripts for build processes and deployment workflows"]},{title:"Software Developer II",company:"Electro Magnetic Applications",period:"February 2022 - March 2023",description:["Led UI/Core team of 4 developers","Integrated CAD visualization APIs","Developed a Linux installer supporting multiple distributions"]},{title:"Software Developer I",company:"Electro Magnetic Applications",period:"February 2021 - February 2022",description:["Developed C#/C++ commercial simulation software","Improved existing applications through algorithm optimization and memory management techniques"]}],r=[{category:"Languages",items:[{name:"TypeScript",icon:W},{name:"Rust",icon:G},{name:"C#",icon:_},{name:"Python",icon:T},{name:"Bash",icon:e1},{name:"Go",icon:L}]},{category:"Frameworks",items:[{name:"React",icon:Q},{name:"Node.js",icon:R},{name:"QML",icon:v},{name:"Qt",icon:v}]},{category:"Databases",items:[{name:"MongoDB",icon:$},{name:"PostgreSQL",icon:X},{name:"MySQL",icon:E}]},{category:"Infrastructure",items:[{name:"AWS",icon:j},{name:"NGINX",icon:K},{name:"Docker",icon:I},{name:"Linux",icon:O},{name:"GitLab CI/CD",icon:w}]}];return e.jsxs("div",{className:"container",children:[e.jsx(i,{children:e.jsxs("div",{className:"about-hero",children:[e.jsx("img",{src:x,alt:"Harrison Oest",className:"about-avatar"}),e.jsxs("div",{className:"about-intro",children:[e.jsx("h1",{children:"Hello!"}),e.jsx("p",{className:"about-title",children:"Lead Back End Engineer"}),e.jsx("p",{className:"about-bio",children:"I'm a skilled developer with expertise in TypeScript, Rust, and cloud infrastructure. I specialize in designing high-availability production services, optimizing system performance, and leading development teams to deliver quality software solutions."})]})]})}),e.jsxs(i,{children:[e.jsx("h2",{children:"Skills"}),r.map(n=>e.jsxs("div",{className:"skill-category",children:[e.jsx("h3",{children:n.category}),e.jsx("div",{className:"skills-grid",children:n.items.map(t=>e.jsxs("span",{className:"skill-tag",children:[e.jsx(t.icon,{size:20,stroke:1.5}),t.name]},t.name))})]},n.category))]}),e.jsxs(i,{children:[e.jsx("h2",{children:"Experience"}),s.map((n,t)=>e.jsxs("div",{className:"experience-item",children:[e.jsxs("div",{className:"experience-header",children:[e.jsxs("div",{children:[e.jsx("h4",{children:n.title}),e.jsx("p",{className:"experience-company",children:n.company})]}),e.jsx("span",{className:"experience-period",children:n.period})]}),e.jsx("ul",{className:"experience-list",children:n.description.map((c,d)=>e.jsx("li",{children:c},d))}),t!==s.length-1&&e.jsx("hr",{className:"divider"})]},n.title))]}),e.jsxs(i,{children:[e.jsx("h2",{children:"Education"}),e.jsxs("div",{className:"experience-header",children:[e.jsxs("div",{children:[e.jsx("h4",{children:"B.S. Computer Science + Business"}),e.jsx("p",{className:"experience-company",children:"Colorado School of Mines"})]}),e.jsx("span",{className:"experience-period",children:"May 2020"})]})]})]})}export{t1 as About};
---
+++ assets/ContentColumn-Bos5hrJ9.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-B-biaQch.js";function n({children:t,style:o}){return s.jsx("section",{className:"surface",style:{marginBottom:"2rem",...o},children:t})}export{n as C};
